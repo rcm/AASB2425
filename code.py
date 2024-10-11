bb5b8db799e873d9f92eaed03f198f15d4dc85ad
Rui Mendes <azuki@di.uminho.pt>
2024-10-11 10:24:28 +0100

    Added the code file

---
+++ code.py
@@ -0,0 +1,65 @@
+import pdb
+
+def revcomp(dna : str) -> str:
+    """
+        Return the reverse complement of a dna string
+
+        dna: str
+            a string of dna (should only have bases ACGT)
+        returns
+            a string with the reverse complement
+    """
+    #reverse = dna[::-1]
+    reverse = ""
+    for base in dna:
+        reverse = base + reverse
+        
+    result = ""
+    for base in reverse:
+        result += complementary_character(base)
+    return result
+def complementary_character_stupid(base: str) -> str:
+    "Gives the complementary base"
+    if base == 'A': return 'T'
+    if base == 'T': return 'A'
+    if base == 'C': return 'G'
+    if base == 'G': return 'C'
+def complementary_character(base: str) -> str:
+    "Gives the complementary base"
+    norm = "ACGT"
+    comp = "TGCA"
+
+    pos = norm.index(base)
+    return comp[pos]
+def complementary_character(base: str) -> str:
+    "Gives the complementary base"
+    norm = "ACGT"
+    comp = "TGCA"
+
+    translation = dict(A = 'T', C = 'G', G = 'C', T = 'A')
+    return translation[base]
+def complementary_character(base: str) -> str:
+    "Gives the complementary base"
+    norm = "ACGT"
+    comp = "TGCA"
+
+    translation = dict(zip(norm, comp))
+    return translation[base]
+
+def get_codons(dna):
+    result = []
+    for pos in range(0, len(dna), 3):
+        codon = dna[pos : pos + 3]
+        if len(codon) == 3:
+            result.append(codon)
+    return result
+
+def codon_to_amino(codons):
+    from table import table
+    res = ''
+    for codon in codons:
+        res += table[codon]
+    return res
+
+if __name__ == '__main__':
+    print("BOOOO!!!!")
